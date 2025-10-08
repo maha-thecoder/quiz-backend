ff48eabab59f22b042bdbf746f23ca71c805e947
Mahanth Narayana Sunkara <surajsunkaraking@gmail.com>
2025-10-08 07:25:22 +0530

    commit successfull

--- controller/control.js
+++ controller/control.js
@@ -1,8 +1,11 @@
 const express=require('express')
 const router=express.Router()
-const {quizquesopt,getquest,gettestname,usertestname,addingscore,addingai,getaitestname}=require('../routes/routes')
+const {quizquesopt,getquest,gettestname,usertestname,addingscore,addingai,getaitestname,userdbadding,
+    userauth
+}=require('../routes/routes')
 
-
+router.route('/signup').post(userdbadding)
+router.route('/signin').post(userauth)
 router.route('/').post(quizquesopt).get(getquest)
 router.route('/quiztopic').get(gettestname)
 router.route('/quiz/:testname').get(usertestname).post(addingscore)
--- models/schema.js
+++ models/schema.js
@@ -1,4 +1,44 @@
 const mongoose=require('mongoose')
+const bcrypt=require('bcrypt')
+
+
+
+
+
+
+
+
+
+
+
+const Userschema=new mongoose.Schema({
+    name:String,
+    email:String,
+    password:String,
+    confirmPassword:String,
+    termsAccepted:Boolean,
+    createdAt:{type:Date,
+    default:Date.now
+    }
+    
+});
+
+
+
+Userschema.pre("save",async function (next){
+    if(!this.isModified("password")) return next(); 
+
+    this.password=await bcrypt.hash(this.password,10);
+    this.confirmPassword=await bcrypt.hash(this.confirmPassword,10)
+    next()
+
+
+})
+
+const Userdb=mongoose.model('Userdb',Userschema)
+
+
+
 
 
 const questionschema=new mongoose.Schema({
@@ -37,4 +77,4 @@ const aischema=new mongoose.Schema({
 })
 
 const aidb =mongoose.model('aidb',aischema)
-module.exports={questionsdb,scoredb,aidb}
+module.exports={questionsdb,scoredb,aidb,Userdb}
