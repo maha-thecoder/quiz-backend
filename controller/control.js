46db781e4aed0022f5fc3d55baac4e36d7e77fc3
Mahanth Narayana Sunkara <surajsunkaraking@gmail.com>
2025-10-16 17:16:52 +0530

    all commits are done

--- controller/control.js
+++ controller/control.js
@@ -1,11 +1,12 @@
 const express=require('express')
 const router=express.Router()
 const {quizquesopt,getquest,gettestname,usertestname,addingscore,addingai,getaitestname,userdbadding,
-    userauth,Proctedroute
+    userauth,Proctedroute,Userdetails
 }=require('../routes/routes')
 
 router.route('/signup').post(userdbadding)
 router.route('/signin').post(userauth)
+router.route('/userprofile/:email').get(Userdetails)
 router.route('/auth').get(Proctedroute)
 router.route('/').post(quizquesopt).get(getquest)
 router.route('/quiztopic').get(gettestname)
