d6118e6a547a6815c6a1edcd8744286be29e8559
Mahanth Narayana Sunkara <surajsunkaraking@gmail.com>
2025-11-07 21:36:03 +0530

    new commit

--- apis/huggingface.js
+++ apis/huggingface.js
@@ -8,7 +8,7 @@ export const tokengenerator = async (req, res) => {
       return res.status(400).json({ error: "Missing question or number of questions" });
     }
 
-    const prompt = `Generate ${no_ques} MCQ questions on ${question}.`;
+    const prompt = `Generate ${no_ques} multiple-choice questions on the topic ${question}. Format the output like this exactly: Question: [question text] A. [option 1] B. [option 2] C. [option 3] D. [option 4] Answer: [Correct option letter] NOTE:Before displaying the answer, check if any answer (or question/option) is in LaTeX format (like \(\frac{x}{2}\) or \sqrt{x}) — if yes, convert it into a normal readable math format (like x/2 or √x) Repeat this format for each question.;`
 
     const response = await fetch("https://router.huggingface.co/v1/chat/completions", {
       method: "POST",
